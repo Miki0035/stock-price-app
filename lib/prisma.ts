e5d1b67a1c61314f60b3a6229d48e54b38582f5e
Mikiyas Kebede <mknewkebede@gmail.com>
2026-05-14 23:24:39 +0300

    fix: change prisma config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
+// import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
